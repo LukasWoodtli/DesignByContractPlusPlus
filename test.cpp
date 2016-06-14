cc2d41f9719371f1cb0a34b153bdbabcb204c801
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-06-14 09:57:26 +0200

    Fix some build errors

--- test.cpp
+++ test.cpp
@@ -1,7 +1,5 @@
 
-
-#include <exception>
-#include <stdexcept>
+#include "include/dbcpp.h"
 	
 #include <iostream>
 
@@ -9,31 +7,7 @@
 
 #include "gtest/gtest.h"
 
-#define _DBC_STRINGIZE(x) #x
-#define _DBC_STR(x) _DBC_STRINGIZE(x)
 
-class DbcException : public std::exception {
-
-public:
-  DbcException(const char* message) : m_message(message) {
-    
-  }
-
-    virtual const char* what() const throw() {return m_message;}
-	
-
-     virtual ~DbcException() throw() {}
-private:
-
-  const char* m_message;
-};
-
-
-void _dbc_general_assert_no_inv(bool cond, const char * msg) {if(!cond) {throw  DbcException(msg);}}
-
-#define DBC_PRECOND_NO_INV(cond) _dbc_general_assert_no_inv(cond, "Precondition failed " #cond " in " __FILE__ " (" _DBC_STR(__LINE__)")");
-
-#define DBC_INV() _dbc_general_assert_no_inv(invariant(), "Invariant check failed in " __FILE__ " (" _DBC_STR(__LINE__)")");
 
 class MyClass {
 
