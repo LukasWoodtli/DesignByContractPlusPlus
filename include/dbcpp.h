36d12c3e2144ab309f0cc52f13c2d6e8e8cb9cb4
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-21 13:12:17 +0200

    Finish first implementation

--- include/dbcpp.h
+++ include/dbcpp.h
@@ -9,26 +9,39 @@
 #define _DBC_STRINGIZE(x) #x
 #define _DBC_STR(x) _DBC_STRINGIZE(x)
 
+
+#define _DBC_FAIL_FUNCT(msg) do{throw  DbcppException(msg);} while(0)
+
+#define _DBC_GENERAL_ASSERT_NO_INVARIANT_CHECK(cond, msg) do {if (!(cond)) {_DBC_FAIL_FUNCT(msg);} } while(0)
+
+
+
+#define DBCPP_PRECOND_NO_INV(cond) do{_DBC_GENERAL_ASSERT_NO_INVARIANT_CHECK(cond, "Precondition failed (" #cond ") in " __FILE__ " (" _DBC_STR(__LINE__)")");} while(0)
+
+#define DBCPP_PRECOND(cond) do { DBCPP_INV(); DBCPP_PRECOND_NO_INV(cond); } while (0)
+
+#define DBCPP_POSTCOND_NO_INV(cond) auto __dbcpp_unique_post ## __LINE__ = _dbcpp_postcond("Postcond failed (" #cond ") at " __FILE__ " ("  _DBC_STR(__LINE__) ")", [&](){return (cond);})
+
+#define DBCPP_POSTCOND(cond) DBCPP_POSTCOND_NO_INV(((cond) && invariant()))
+
+#define DBCPP_INV() do {_DBC_GENERAL_ASSERT_NO_INVARIANT_CHECK(invariant(), "Invariant check failed in " __FILE__ " (" _DBC_STR(__LINE__)")");} while(0)
+
+
+
+
+
+
 class DbcppException : public std::exception {
 
 public:
-  explicit DbcppException(const char* message) : m_message(message) {
-    
-  }
+  explicit DbcppException(const char* message) : m_message(message) {}
 
     virtual const char* what() const throw() {return m_message;}
-	
-
-     virtual ~DbcppException() throw() {}
+	virtual ~DbcppException() throw() {}
 private:
 
   const char* m_message;
 };
-
-
-
-void _dbcpp_general_assert_no_inv(bool cond, const char * msg);
-
 
 
 
@@ -40,9 +53,9 @@ public:
     {}
     
     ~_dbcpp_postcond() {
-        if( !std::uncaught_exception() && !m_f() )
+        if( !std::uncaught_exception())
         {
-            throw DbcppException(m_msg);
+            _DBC_GENERAL_ASSERT_NO_INVARIANT_CHECK(m_f(), m_msg);
         }
     }
     
@@ -51,12 +64,6 @@ private:
     char const * const m_msg;
 };
 
-#define DBCPP_PRECOND_NO_INV(cond) _dbcpp_general_assert_no_inv(cond, "Precondition failed (" #cond ") in " __FILE__ " (" _DBC_STR(__LINE__)")");
-
-#define DBC_INV() _dbc_general_assert_no_inv(invariant(), "Invariant check failed in " __FILE__ " (" _DBC_STR(__LINE__)")");
-
-
-#define DBCPP_POSTCOND_NO_INV(cond) auto __dbcpp_unique_post = _dbcpp_postcond("Postcond failed (" #cond ") at " __FILE__ " ("  _DBC_STR(__LINE__) ")", [&](){return (cond);})
 
 
 
--- test.cpp
+++ test.cpp
@@ -8,17 +8,44 @@
 
 
 
+
+// DBCPP_POSTCOND_NO_INV(cond)
+// DBCPP_POSTCOND(cond) DBCPP_POSTCOND_NO_INV(cond && DBC_INV)
+
 class PrecondExampleClass {
 
 public:
     PrecondExampleClass() : m_a(0) {}
 
-  void precondFailingMethod() {
-    DBCPP_PRECOND_NO_INV(m_a == 1);
+  void precondFailingNoInvMethod(int x) {
+    DBCPP_PRECOND_NO_INV(x != 5);
   }
 
-  void precondSuccessMethod() {
-    DBCPP_PRECOND_NO_INV(m_a == 0);
+  void precondSuccessNoInvMethod(int x) {
+    DBCPP_PRECOND_NO_INV(x == 5);
+  }
+    
+  void precondFailingInvOkMethod(int x) {
+    DBCPP_PRECOND(x != 5);
+  }
+    
+  void precondSuccessInvFailingMethod(int x) {
+    m_a = 1;
+    DBCPP_PRECOND(x == 5);
+  }
+
+  void precondFailingInvFailingMethod(int x) {
+    m_a = 1;
+    DBCPP_PRECOND(x != 5);
+  }
+    
+  void precondOkInvOkMethod(int x) {
+      DBCPP_PRECOND(x == 5);
+  }
+    
+private:
+  bool invariant() const {
+    return m_a == 0;
   }
 
 private:
@@ -30,29 +57,81 @@ private:
 TEST(PreconditionTest, precondFail)
 {
   PrecondExampleClass c;
-  ASSERT_THROW(c.precondFailingMethod(), DbcppException);
+  ASSERT_THROW(c.precondFailingNoInvMethod(5), DbcppException);
 }
 
 TEST(PreconditionTest, precondSuccess)
 {
     PrecondExampleClass c;
-    ASSERT_NO_THROW(c.precondSuccessMethod());
+    ASSERT_NO_THROW(c.precondSuccessNoInvMethod(5));
+}
+
+TEST(PreconditionTest, precondFailingInvOk)
+{
+    PrecondExampleClass c;
+    ASSERT_THROW(c.precondFailingInvOkMethod(5), DbcppException);
 }
 
 
+TEST(PreconditionTest, precondSuccessInvFailing)
+{
+    PrecondExampleClass c;
+    ASSERT_THROW(c.precondSuccessInvFailingMethod(5), DbcppException);
+}
+
+
+TEST(PreconditionTest, precondFailingInvFailing)
+{
+    PrecondExampleClass c;
+    ASSERT_THROW(c.precondFailingInvFailingMethod(5), DbcppException);
+}
+
+TEST(PreconditionTest, precondOkInvOk)
+{
+    PrecondExampleClass c;
+    ASSERT_NO_THROW(c.precondOkInvOkMethod(5));
+}
+
+
+
+// DBCPP_PRECOND_NO_INV(cond)
+// DBCPP_PRECOND(cond)
 class PostcondExampleClass {
     
 public:
     PostcondExampleClass() : m_a(0) {}
     
-    void postcondFailingMethod() {
+    void postcondFailingNoInvMethod() {
         DBCPP_POSTCOND_NO_INV(m_a == 1);
         
     }
     
-    void postcondSuccessMethod() {
+    void postcondSuccessNoInvMethod() {
         DBCPP_POSTCOND_NO_INV(m_a == 0);
     }
+    
+    void postcondFailingInvOkMethod(int x) {
+        DBCPP_POSTCOND(x != 5);
+    }
+    
+    void postcondSuccessInvFailingMethod(int x) {
+      m_a = 1;
+      DBCPP_POSTCOND(x == 5);
+    }
+
+    void postcondFailingInvFailingMethod(int x) {
+      m_a = 1;
+      DBCPP_POSTCOND(x != 5);
+    }
+    
+    void postcondSuccessInvOkMethod(int x) {
+      DBCPP_POSTCOND(x == 5);
+    }
+
+private:
+     bool invariant() const {
+       return m_a == 0;
+     }
     
 private:
     int m_a;
@@ -62,13 +141,75 @@ private:
 TEST(PostconditionTest, postcondFail)
 {
     PostcondExampleClass c;
-    ASSERT_DEATH(c.postcondFailingMethod(), "Postcond failed");
+    ASSERT_DEATH(c.postcondFailingNoInvMethod(), "Postcond failed");
 }
 
 TEST(PostconditionTest, postcondSuccess)
 {
     PostcondExampleClass c;
-    ASSERT_NO_THROW(c.postcondSuccessMethod());
+    ASSERT_NO_THROW(c.postcondSuccessNoInvMethod());
+}
+
+TEST(PostconditionTest, postcondSuccessInv)
+{
+  PostcondExampleClass c;
+  ASSERT_DEATH(c.postcondSuccessInvFailingMethod(5), "Postcond failed");
 }
 
 
+TEST(PostconditionTest, postcondFailingInvOk)
+{
+    PostcondExampleClass c;
+    ASSERT_DEATH(c.postcondFailingInvOkMethod(5), "Postcond failed");
+}
+
+
+TEST(PostconditionTest, postcondFailingInvFailing)
+{
+    PostcondExampleClass c;
+    ASSERT_DEATH(c.postcondFailingInvFailingMethod(5), "Postcond failed");
+}
+
+
+TEST(PostconditionTest, postcondSuccessInvOk)
+{
+    PostcondExampleClass c;
+    ASSERT_NO_THROW(c.postcondSuccessInvOkMethod(5));
+}
+
+// DBCPP_INV()
+
+class InvariantExampleClass {
+    
+public:
+    InvariantExampleClass() : m_a(0) {}
+
+    void invOkMethod() {
+      DBCPP_INV();
+    }
+    
+    void invFailMethod() {
+      m_a = 1;
+      DBCPP_INV();
+    }
+
+private:
+  bool invariant() const {
+    return m_a == 0;
+  }
+
+private:
+  int m_a;
+};
+
+TEST(InvariantTest, invOk)
+{
+    InvariantExampleClass c;
+    ASSERT_NO_THROW(c.invOkMethod());
+}
+
+TEST(InvariantTest, invFail)
+{
+    InvariantExampleClass c;
+    ASSERT_THROW(c.invFailMethod(), DbcppException);
+}
